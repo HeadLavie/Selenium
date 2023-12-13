c94ac836df59aed52dca616cfd897969d2212ff7
lavie head <nadya.karpenko@gmail.com>
2023-12-13 23:56:15 +0000

    WebDriver

--- src/test/java/ru/netology/web/CallbackTest.java
+++ src/test/java/ru/netology/web/CallbackTest.java
@@ -20,7 +20,7 @@ class CallbackTest {
 
     @BeforeAll
     static void setUpAll() {
-        System.setProperty("webdriver.chrome.driver","driver/win/chromedriver.exe");
+        WebDriverManager.chromedriver().setup();
     }
 
     @BeforeEach
