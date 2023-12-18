1dbeb2d402e76acdbf6bc3f5d670bcbf41bf7ec9
lavie head <nadya.karpenko@gmail.com>
2023-12-18 22:59:47 +0000

    Homework submission

--- src/test/java/ru/netology/web/CallbackTest.java
+++ src/test/java/ru/netology/web/CallbackTest.java
@@ -47,35 +47,35 @@ class CallbackTest {
     void shouldTestV1() throws InterruptedException {
         driver.get("http://localhost:7777/");
         Thread.sleep(2000);
-        WebElement form = driver.findElement(By.className("form_theme_alfa-on-white"));
-        List<WebElement> elements = driver.findElements(By.className("input_control"));
+        List<WebElement> elements = driver.findElements(By.className("input__control"));
         elements.get(0).sendKeys("Надежда");
         elements.get(1).sendKeys("+11122233345");
-        driver.findElement(By.className("checkbox_box")).click();
+        driver.findElement(By.className("checkbox__box")).click();
+        driver.findElement(By.className("button")).click();
+        String text = driver.findElement(By.className("Success_successBlock__2L3Cw")).getText();
+        assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", text.trim());
+    }
+
+    @Test
+    void shouldTestV2() throws InterruptedException {
+        driver.get("http://localhost:7777/");
+        List<WebElement> elements = driver.findElements(By.className("input__control"));
+        elements.get(0).sendKeys("Надежда Карпенко");
+        elements.get(1).sendKeys("+11122233345");
+        driver.findElement(By.className("checkbox__box")).click();
         driver.findElement(By.className("button")).click();
         String text = driver.findElement(By.cssSelector("[data-test-id=order-success]")).getText();
         assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", text.trim());
     }
-    @Test
-    void shouldTestV2() throws InterruptedException {
-        driver.get("http://localhost:7777/");
-        Thread.sleep(2000);
-        WebElement form = driver.findElement(By.className("form_theme_alfa-on-white"));
-        form.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Надежда Карпенко");
-        form.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+11122233345");
-        form.findElement(By.cssSelector("[data-test-id=agreement]")).click();
-        form.findElement(By.className("button_theme_alfa-on-white")).click();
-        String text = driver.findElement(By.cssSelector("[data-test-id=order-success]")).getText();
-        assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", text.trim());
-    }
+
     @Test
     void shouldTestV3() {
         driver.get("http://localhost:7777/");
-        WebElement form = driver.findElement(By.className("form_theme_alfa-on-white"));
-        form.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Nick");
-        form.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+11122233345");
-        form.findElement(By.cssSelector("[data-test-id=agreement]")).click();
-        form.findElement(By.className("button_theme_alfa-on-white")).click();
+        List<WebElement> elements = driver.findElements(By.className("input__control"));
+        elements.get(0).sendKeys("Nick");
+        elements.get(1).sendKeys("+11122233345");
+        driver.findElement(By.className("checkbox__box")).click();
+        driver.findElement(By.className("button")).click();
         String text = driver.findElement(By.cssSelector("[data-test-id=name] .input__sub")).getText();
         assertEquals("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы.", text.trim());
     }
@@ -83,11 +83,11 @@ class CallbackTest {
     @Test
     void shouldTestV4() {
         driver.get("http://localhost:7777/");
-        WebElement form = driver.findElement(By.className("form_theme_alfa-on-white"));
-        form.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Надежда");
-        form.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("0894448773");
-        form.findElement(By.cssSelector("[data-test-id=agreement]")).click();
-        form.findElement(By.className("button_theme_alfa-on-white")).click();
+        List<WebElement> elements = driver.findElements(By.className("input__control"));
+        elements.get(0).sendKeys("Надежда Карпенко");
+        elements.get(1).sendKeys("0894448773");
+        driver.findElement(By.className("checkbox__box")).click();
+        driver.findElement(By.className("button")).click();
         String text = driver.findElement(By.cssSelector("[data-test-id=phone] .input__sub")).getText();
         assertEquals("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678.", text.trim());
     }
@@ -95,15 +95,14 @@ class CallbackTest {
     @Test
     void shouldTestV5() {
         driver.get("http://localhost:7777/");
-        WebElement form = driver.findElement(By.className("form_theme_alfa-on-white"));
-        form.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Надежда");
-        form.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+11122233345");
+        List<WebElement> elements = driver.findElements(By.className("input__control"));
+        elements.get(0).sendKeys("Надежда");
+        elements.get(1).sendKeys("+11122233345");
 
         WebElement labelElement = driver.findElement(By.cssSelector("[data-test-id=agreement]"));
         String initialClass = labelElement.getAttribute("class");
 
-
-        form.findElement(By.className("button_theme_alfa-on-white")).click();
+        driver.findElement(By.className("button_theme_alfa-on-white")).click();
 
         // Wait for the class to change
         Duration timeout = Duration.ofSeconds(10);
@@ -114,9 +113,5 @@ class CallbackTest {
 
         // Assert that the class has changed as expected
         assertTrue(updatedClass.contains("input_invalid"), "Class has not changed as expected");
-
-
-
     }
-
 }
